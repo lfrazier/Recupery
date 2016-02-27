ed606ae2f0a44a3c77b5728e4e0c125394b4d779
zachdorcinville21 <zacharydorcinville@zacharys-air.lan.ousd.org>
2016-02-27 15:23:54 -0800

    login layout

---
+++ Recupery/Recupery/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Recupery
+//
+//  Created by Zachary Dorcinville on 2/27/16.
+//  Copyright © 2016 Recupery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
